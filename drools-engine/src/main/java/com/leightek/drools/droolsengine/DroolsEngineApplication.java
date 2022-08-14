f197e2781dfd49a69355ada0aad7d2cd7ce99890
leightek <leighsuconsulting@gmail.com>
2022-08-14 16:33:35 -0700

    [SpringBoot] Added Drools engine example

---
+++ drools-engine/src/main/java/com/leightek/drools/droolsengine/DroolsEngineApplication.java
@@ -0,0 +1,13 @@
+package com.leightek.drools.droolsengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolsEngineApplication.class, args);
+	}
+
+}
